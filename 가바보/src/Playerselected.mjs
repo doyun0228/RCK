f3054838a79d79e81b1e578f9845db66e035ad19
doyun0228 <162782073+doyun0228@users.noreply.github.com>
2024-03-30 15:41:25 +0900

    Delete 가바보 directory

--- 가바보/src/Playerselected.mjs
+++
@@ -1,7 +0,0 @@
-export function Playerselected(Pslected) {
-  const back = document.querySelector(".background");
-  const Pselect = document.createElement("h1");
-  back.appendChild(Pselect);
-  Pselect.className = "ps";
-  Pselect.innerText = "당신은 " + Pslected + "를 선택하였습니다";
-}
--- 가바보/src/constants.mjs
+++
@@ -1,3 +0,0 @@
-export const ROCK = 1;
-export const PAPER = 2;
-export const SCISSOR = 3;
